644253f67f897026c1babd58f4ad42d725e41f82
deviant96 <mcrealdeviant@gmail.com>
2024-06-16 05:22:19 +0700

    Re-do design and apply mobile styling

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mobile/main.css') }}" media="only screen and (max-width: 600px)">
+    <link rel="stylesheet" href="{{ asset('css/tablet/main.css') }}"
+        media="only screen and (min-width: 601px) and (max-width: 1024px)">
+    <link rel="stylesheet" href="{{ asset('css/desktop/main.css') }}" media="only screen and (min-width: 1025px)">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Task app</title>
@@ -13,6 +16,17 @@
 </head>
 
 <body>
+    @if (auth()->check())
+        <nav class="nav">
+            <div>Hi, {{ Auth::user()->username }}</div>
+            <div>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="logout-button">Logout</button>
+                </form>
+            </div>
+        </nav>
+    @endif
     <div class="main">
         @yield('content')
     </div>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,256 +1,172 @@
 @extends('layouts.layout')
 
 @section('styles')
-    <style>
-        h2,
-        h3 {
-            color: #333;
-            margin-bottom: 20px;
-        }
+<style>
+    .close-btn {
+        position: absolute;
+        right: 0;
+        top: 0;
+        transform: translate(-85%, 125%);
+        border: none;
+        background: transparent;
+        color: gray;
+        cursor: pointer;
+    }
 
-        form {
-            margin-bottom: 30px;
-        }
-
-        label {
-            display: block;
-            font-weight: bold;
-        }
-
-        input,
-        select {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 15px;
-            /* border: 1px solid #ddd; */
-            /* border-radius: 5px; */
-            font-size: 16px;
-            color: #555;
-            background-color: #f9f9f9;
-        }
-
-        button {
-            width: 100%;
-            padding: 12px;
-            background: var(--primary-color);
-            color: white;
-            border: none;
-            border-radius: 5px;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background 0.3s;
-        }
-
-        button:hover {
-            background: #0056b3;
-        }
-
-        .clear-completed {
-            background: #dc3545;
-            margin-bottom: 20px;
-        }
-
-        .clear-completed:hover {
-            background: #c82333;
-        }
-
-        .form-group select {
-            width: 100%;
-            padding: 0.5rem 2.5rem 0.5rem 0.5rem;
-            border: none;
-            border-bottom: 1px solid #ccc;
-            font-size: 1rem;
-            outline: none;
-            transition: border-color 0.3s;
-        }
-
-        .form-group select:focus {
-            border-bottom: 2px solid var(--primary-color);
-        }
-
-        .form-group select+.placeholder {
-            position: absolute;
-            left: 0.5rem;
-            top: 50%;
-            transform: translateY(-50%);
-            font-size: 1rem;
-            color: #aaa;
-            transition: all 0.3s;
-            pointer-events: none;
-
-            top: 10px;
-            left: 0.5rem;
-            font-size: 0.75rem;
-            color: var(--primary-color);
-        }
-
-        #taskList {
-            list-style-type: none;
-        }
-
-        #taskList li {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 15px;
-            margin-bottom: 10px;
-            background: #f1f1f1;
-            border-radius: 5px;
-            border-left: 5px solid var(--primary-color);
-        }
-
-        #taskList li.completed {
-            text-decoration: line-through;
-            color: #888;
-            border-left-color: #28a745;
-        }
-
-        #taskList li form {
-            display: inline;
-        }
-
-        .task-actions {
-            display: flex;
-            gap: 10px;
-        }
-
-        .icon-button {
-            background: none;
-            border: none;
-            cursor: pointer;
-            color: var(--primary-color);
-            font-size: 1.2em;
-            transition: color 0.3s;
-        }
-
-        .icon-button:hover {
-            color: #0056b3;
-        }
-
-        .icon-button .fa-trash {
-            color: #dc3545;
-        }
-
-        .icon-button:hover .fa-trash {
-            color: #c82333;
-        }
-
-        .clear-completed:disabled {
-            background: #ddd;
-            cursor: not-allowed;
-        }
-
-        .logout-button {
-            background: #dc3545;
-        }
-
-        .logout-button:hover {
-            background: #c82333;
-        }
-    </style>
+    .close-btn:hover {
+        color: #000;
+    }
+</style>
 @endsection
 
 @section('content')
-    <div class="container">
-        @if (auth()->check())
-            <form method="POST" action="{{ route('logout') }}">
+<div class="container task-container">
+    <div id="overlay" class="hidden">
+        <div id="formContainer" class="form-hidden">
+            <button id="closeFormBtn" class="close-btn">Close</i></button>
+            <h2>Tasks</h2>
+            <form id="taskForm" method="POST" action="{{ route('tasks.store') }}">
                 @csrf
-                <button type="submit" class="logout-button">Logout</button>
+                <x-input type="text" name="description" id="description" label="Description"
+                    value="{{ old('description') }}" icon="fa fa-check" required />
+
+                <x-input type="date" name="due_date" id="due_date" label="Due Date" value="{{ old('due_date') }}"
+                    required style="padding-right: 4px" />
+
+                <div class="form-group">
+                    <select class="form-control" id="priority" name="priority" required>
+                        <option value="Low" selected>Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                    </select>
+                    <label for="priority" class="placeholder">Priority:</label>
+                </div>
+
+                <button class="button" type="submit">Add Task</button>
             </form>
-        @endif
-        <h2>Tasks</h2>
-        <form id="taskForm" method="POST" action="{{ route('tasks.store') }}">
-            @csrf
-            <x-input type="text" name="description" id="description" label="Description" value="{{ old('description') }}"
-                icon="fa fa-user" required />
-
-            <x-input type="date" name="due_date" id="due_date" label="Due Date" value="{{ old('due_date') }}" required
-                style="padding-right: 4px" />
-
-            <div class="form-group">
-                <select class="form-control" id="priority" name="priority" required>
-                    <option value="Low" selected>Low</option>
-                    <option value="Medium">Medium</option>
-                    <option value="High">High</option>
-                </select>
-                <label for="priority" class="placeholder">Priority:</label>
-            </div>
-
-
-            <button type="submit">Add Task</button>
-        </form>
-
-        <h3>My Tasks</h3>
-        <button id="clearCompleted" class="clear-completed">Clear All Completed</button>
-        <ul id="taskList">
-            @foreach ($tasks as $task)
-                <li class="{{ $task->is_completed ? 'completed' : '' }}">
-                    <span>{{ $task->description }} - {{ $task->due_date }} - {{ $task->priority }}</span>
-                    <div class="task-actions">
-                        <form method="POST" action="{{ route('tasks.update', $task->id) }}" style="display: inline;">
-                            @csrf
-                            @method('PUT')
-                            <button type="submit" class="icon-button"
-                                title="{{ $task->is_completed ? 'Mark Incomplete' : 'Mark Complete' }}">
-                                <i class="fas {{ $task->is_completed ? 'fa-undo' : 'fa-check' }}"></i>
-                            </button>
-                        </form>
-                        <form method="POST" action="{{ route('tasks.destroy', $task->id) }}" class="delete-form"
-                            style="display: inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="icon-button" title="Delete Task">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </form>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
+        </div>
     </div>
+
+    <div class="task-list">
+        <header class="task-list-header">
+            <h3>My Tasks</h3>
+            <div id="taskMenuAction" class="task-menu-actions">
+                <button id="clearCompleted" class="clear-completed">Clear All Completed</button>
+                <button id="newTaskBtn" class="new-task">New task</button>
+            </div>
+        </header>
+
+        <main>
+            <div id="alertDueToday" class="alert-due-today" style="display: none;">You have tasks due today!</div>
+            <ul id="taskList">
+                @foreach ($tasks as $task)
+                    <li
+                        class="task-item {{ \Carbon\Carbon::parse($task->due_date)->lessThanOrEqualTo(\Carbon\Carbon::today()) ? 'due-today' : '' }} {{ $task->is_completed ? 'completed' : '' }}">
+                        <div class="task-content">
+                            <h4 class="task-description">{{ $task->description }}</h4>
+                            <p class="task-priority">{{ $task->priority }}</p>
+                            <p class="task-due">{{ $task->due_date }}</p>
+                        </div>
+                        <div class="task-actions">
+                            <form method="POST" action="{{ route('tasks.destroy', $task->id) }}" class="delete-form"
+                                style="display: inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="icon-button" title="Delete Task">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                            <form method="POST" action="{{ route('tasks.update', $task->id) }}" style="display: inline;">
+                                @csrf
+                                @method('PUT')
+                                <button type="submit" class="icon-button"
+                                    title="{{ $task->is_completed ? 'Mark Incomplete' : 'Mark Complete' }}">
+                                    <i class="fas {{ $task->is_completed ? 'fa-undo' : 'fa-check' }}"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </main>
+    </div>
+</div>
 @endsection
 
 @section('scripts')
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const clearCompletedButton = document.getElementById('clearCompleted');
-            const completedTasks = document.querySelectorAll('#taskList li.completed');
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
 
-            if (completedTasks.length === 0) {
-                clearCompletedButton.disabled = true;
+        const clearCompletedButton = document.getElementById('clearCompleted');
+        const completedTasks = document.querySelectorAll('#taskList li.completed');
+        const dueTodayTasks = document.querySelectorAll('#taskList li.due-today:not(.completed)');
+        const alertDueToday = document.getElementById('alertDueToday');
+
+        if (completedTasks.length === 0) {
+            clearCompletedButton.disabled = true;
+        }
+
+        if (dueTodayTasks.length > 0) {
+            alertDueToday.style.display = 'block';
+        }
+
+        clearCompletedButton.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            if (confirm('Are you sure you want to clear all completed tasks?')) {
+                fetch('/tasks/clear-completed', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')
+                            .getAttribute('content')
+                    },
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            window.location.reload();
+                        } else {
+                            alert(data.message);
+                        }
+                    })
+                    .catch(error => console.error('Error:', error));
             }
+        });
 
-            clearCompletedButton.addEventListener('click', function(event) {
-                event.preventDefault();
-
-                if (confirm('Are you sure you want to clear all completed tasks?')) {
-                    fetch('/tasks/clear-completed', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')
-                                    .getAttribute('content')
-                            },
-                        })
-                        .then(response => response.json())
-                        .then(data => {
-                            if (data.success) {
-                                window.location.reload();
-                            } else {
-                                alert(data.message);
-                            }
-                        })
-                        .catch(error => console.error('Error:', error));
+        document.querySelectorAll('.delete-form').forEach(form => {
+            form.addEventListener('submit', function (event) {
+                if (!confirm('Are you sure you want to delete this task?')) {
+                    event.preventDefault();
                 }
             });
-
-            document.querySelectorAll('.delete-form').forEach(form => {
-                form.addEventListener('submit', function(event) {
-                    if (!confirm('Are you sure you want to delete this task?')) {
-                        event.preventDefault();
-                    }
-                });
-            });
         });
-    </script>
+
+        const buttonNewTask = document.getElementById('newTaskBtn');
+        const buttonCloseForm = document.getElementById('closeFormBtn');
+        const formContainer = document.getElementById('formContainer');
+        const overlay = document.getElementById('overlay');
+        buttonNewTask.addEventListener('click', function () {
+            overlay.classList.remove('hidden');
+            formContainer.classList.toggle('show');
+            // document.body.style.filter = 'blur(5px)';
+        });
+
+        buttonCloseForm.addEventListener('click', function () {
+            closeForm();
+        });
+
+        overlay.addEventListener('click', function (event) {
+            if (event.target === this) {
+                closeForm();
+            }
+        });
+
+        function closeForm() {
+            overlay.classList.add('hidden');
+            formContainer.classList.remove('show');
+            document.getElementById('taskForm').reset();
+        }
+    });
+</script>
 @endsection
