b5e3d0c3bbdf64aeace9dc893af210bf2f1562f2
deviant96 <mcrealdeviant@gmail.com>
2024-06-16 09:12:25 +0700

    Add font

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,6 +9,11 @@
         media="only screen and (min-width: 601px) and (max-width: 1024px)">
     <link rel="stylesheet" href="{{ asset('css/desktop/main.css') }}" media="only screen and (min-width: 1025px)">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Task app</title>
 
